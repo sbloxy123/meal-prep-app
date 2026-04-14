ba268c0c6ab153955bc0fa7e6c3efdcc32630f87
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2026-04-14 22:41:25 +0100

    removed node variable for production check

--- db/pool.js
+++ db/pool.js
@@ -3,16 +3,17 @@ const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = new Pool(
-    isProduction
-        ? {
-              connectionString: process.env.DATABASE_URL,
-              ssl: { rejectUnauthorized: false },
-          }
-        : {
-              host: process.env.HOST,
-              user: process.env.USER,
-              database: process.env.DATABASE,
-              password: process.env.PASSWORD,
-              port: process.env.DATABASE_PORT,
-          },
+    // isProduction
+    //     ? {
+    //           connectionString: process.env.DATABASE_URL,
+    //           ssl: { rejectUnauthorized: false },
+    //       }
+    //     :
+    {
+        host: process.env.HOST,
+        user: process.env.USER,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        port: process.env.DATABASE_PORT,
+    },
 );
